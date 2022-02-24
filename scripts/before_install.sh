28491cea4d86e820172492bbb12a32135663854c
skssandy <89083444+skssandy@users.noreply.github.com>
2022-02-24 17:47:18 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -11,7 +11,7 @@ sudo apt-get install nodejs -y
 
 # Install forever module 
 # https://www.npmjs.com/package/forever
-sudo npm install forever -g
+# sudo npm install forever -g
 
 # Clean working folder
 # sudo find /home/ubuntu/test -type f -delete
